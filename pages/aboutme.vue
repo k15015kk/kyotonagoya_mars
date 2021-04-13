5c8f00716cf6cc554f2e8c696b113994aae2cb85
k15015kk <b19702bb@aitech.ac.jp>
2021-04-13 23:41:41 +0900

    自己紹介文の変更

--- pages/aboutme.vue
+++ pages/aboutme.vue
@@ -11,13 +11,18 @@
           <v-card-text>
             <h1 class="text-h4 text-center pb-4">Haruki Inoue</h1>
             <p>
-              情報科学を専攻している大学院修士2年です．公共交通機関と位置情報アプリを絡めた研究をしています．最近はスマートフォアプリの開発とWebアプリケーションの開発をしています．週末は公共交通機関を使ってお出かけをしています．乗ることがお出かけのメインです．あと，よくヒトカラに行きます．2021年から千葉県内のどこかに住みます．
+              千葉県流山市に住んでおり，東京に出勤している社会人初心者です．位置情報のアプリケーションに関する仕事をしています．最近はスマートフォアプリの開発とWebアプリケーションの開発をしています．週末は公共交通機関を使ってお出かけをしています．乗ることがお出かけのメインです．あと，よくヒトカラに行きます．
             </p>
             <h5 class="text-h5">Profile</h5>
-            <div class="pt-2 pb-2">
+            <!-- <div class="pt-2 pb-2">
               <span class="font-weight-bold pr-4">所属</span>
               <br />
-              愛知工業大学大学院 経営情報科学専攻 修士2年
+              東京のどこかにいます
+            </div> -->
+            <div class="pt-2 pb-2">
+              <span class="font-weight-bold pr-4">居住地</span>
+              <br />
+              千葉県流山市
             </div>
             <div class="pt-2 pb-2">
               <span class="font-weight-bold pr-4">出身地</span>
@@ -44,10 +49,13 @@
             </div>
             <div class="pt-2 pb-2">
               <span class="font-weight-bold pr-4">好きな電車</span>
-              <br />多すぎて決められない
+              <br />最近は つくばエクスプレス
+              によく乗っています．どちらかというと新しい列車の方が好きですが，古い列車も乗りに行きます．
             </div>
             <div class="pt-2 pb-2">
-              <span class="font-weight-bold pr-4">使ってる言語・技術</span>
+              <span class="font-weight-bold pr-4"
+                >いままで使っていた言語・技術</span
+              >
               <ul>
                 <li>Swift(iOS)</li>
                 <li>Kotlin(Android)</li>
